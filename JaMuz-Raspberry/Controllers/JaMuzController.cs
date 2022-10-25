c6039ef0284b777813b18ef41cc9c6fe424264d8
Raphael Camus <raphael.camus.fr@non.se.com>
2022-10-25 20:28:46 +0200

    fix swagger

--- JaMuz-Raspberry/Controllers/JaMuzController.cs
+++ JaMuz-Raspberry/Controllers/JaMuzController.cs
@@ -1,4 +1,5 @@
 ﻿using JaMuzRaspberry.Database.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Logging;
@@ -8,7 +9,7 @@ using System.Linq;
 namespace JaMuzRaspberry.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api")]
 public class JaMuzController : ControllerBase
 {
     private readonly ILogger<JaMuzController> _logger;
@@ -18,24 +19,22 @@ public class JaMuzController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet]
-    public List<File> Get()
+    [HttpGet("marley")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<File>))]
+    public IActionResult Get()
     {
         try
         {
-            using (var db = new Database.JaMuzContext())
-            {
-                var files = db.File
-                    .Where(b => b.Rating > 3 && b.Artist.Contains("Marley"))
-                    .OrderBy(b => b.TrackNo)
-                    .ToList();
-
-                return files;
-            }
+            using var db = new Database.JaMuzContext();
+            var files = db.File
+                .Where(b => b.Rating > 3 && b.Artist.Contains("Marley"))
+                .OrderBy(b => b.TrackNo)
+                .ToList();
+            return Ok(files);
         } catch(SqliteException ex)
         {
             _logger.LogError(ex, "Error in Get");
-            return null;
+            return NotFound(ex.Message);
         }
     }
 }
