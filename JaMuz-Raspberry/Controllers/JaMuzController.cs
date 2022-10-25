500e4009401d9c37c95144205030443c02176a20
Raphael Camus <raphael.camus.fr@non.se.com>
2022-10-25 21:09:19 +0200

    audio player POC

--- JaMuz-Raspberry/Controllers/JaMuzController.cs
+++ JaMuz-Raspberry/Controllers/JaMuzController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Logging;
+using NAudio.Wave;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,6 +14,8 @@ namespace JaMuzRaspberry.Controllers;
 public class JaMuzController : ControllerBase
 {
     private readonly ILogger<JaMuzController> _logger;
+    private static WaveOutEvent outputDevice;
+    private static AudioFileReader audioFile;
 
     public JaMuzController(ILogger<JaMuzController> logger)
     {
@@ -36,5 +39,43 @@ public class JaMuzController : ControllerBase
             _logger.LogError(ex, "Error in Get");
             return NotFound(ex.Message);
         }
+    }
+
+    [HttpGet("play/{id}")]
+    public IActionResult PlayFile([FromRoute] int id)
+    {
+        PlayFile();
+        return Ok();
+    }
+
+    [HttpGet("/stop")]
+    public IActionResult StopFile()
+    {
+        outputDevice?.Stop();
+        return Ok();
+    }
+
+    private void PlayFile()
+    {
+        var filename = @"C:\Users\xxxx.mp3";
+        if (outputDevice == null)
+        {
+            outputDevice = new WaveOutEvent();
+            outputDevice.PlaybackStopped += OnPlaybackStopped;
+        }
+        if (audioFile == null)
+        {
+            audioFile = new AudioFileReader(filename);
+            outputDevice.Init(audioFile);
+        }
+        outputDevice.Play();
+    }
+
+    private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+    {
+        outputDevice.Dispose();
+        outputDevice = null;
+        audioFile.Dispose();
+        audioFile = null;
     }
 }
